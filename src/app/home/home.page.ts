9c85e533776e0b8386c72908e12cc62c44188be3
garrisonholt <gholt29@gmail.com>
2022-08-20 23:38:52 -0500

    Added Folder

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ModalController} from "@ionic/angular";
+import {AddNewTaskPage} from "../add-new-task/add-new-task.page";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todoList = [];
+  today: number = Date.now();
 
-  constructor() {}
+  constructor(public modalCtrl: ModalController) {
+  }
 
+  //Adding a task function to the homepage
+  async addTask() {
+    const modal = await this.modalCtrl.create({
+      component: AddNewTaskPage
+    });
+
+    modal.onDidDismiss().then(newTaskObj => {
+      console.log(newTaskObj);
+      this.todoList.push(newTaskObj.data);
+    });
+    return await modal.present();
+  }
+
+  //Deleteing the item by splicing the index
+  delete(index) {
+    this.todoList.splice(index,1);
+  }
+
+  //This function utilizes the same concept as the delete function
+  complete(index) {
+    this.todoList.splice(index, 1);
+  }
 }
